62ea2147f9c1f673e72b83134b0fb3c86799768d
GH15ADF <synnoack@gmail.com>
2020-07-21 11:08:21 -0400

    Set offset assuming 1.25mm disk platter

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -956,7 +956,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1096,8 +1096,8 @@
 #define Y_BED_SIZE 440
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -30
-#define Y_MIN_POS -30
+#define X_MIN_POS 0
+#define Y_MIN_POS 0
 #define Z_MIN_POS -30
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
@@ -1311,7 +1311,7 @@
  * Add a bed leveling sub-menu for ABL or MBL.
  * Include a guided procedure if manual probing is enabled.
  */
-//#define LCD_BED_LEVELING
+#define LCD_BED_LEVELING
 
 #if ENABLED(LCD_BED_LEVELING)
   #define MESH_EDIT_Z_STEP  0.025 // (mm) Step size while manually probing Z axis.
