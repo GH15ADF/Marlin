03c15be8485256014ac28db9be062441e831d13a
GH15ADF <synnoack@gmail.com>
2020-07-21 21:04:25 -0400

    Tweak sensorless homing again

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1364,8 +1364,8 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (4*60)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_XY (8*60)
+#define HOMING_FEEDRATE_Z  (8*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
